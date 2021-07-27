d592d37ae2eea4d536df48ad26f17eb929e25f0b
nikit <nikit@DESKTOP-8GA9F74>
2021-07-27 19:31:11 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static HubConnection HubConnection;
+        static async Task Main(string[] args)
+        {
+            HubConnection = new HubConnectionBuilder().WithUrl("http://localhost:19929/notification").Build();
+            HubConnection.On<String>("Send", message => Console.WriteLine($"Message from server: { message}"));
+
+            await HubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+
+                if (message != "Exit")
+                {
+                    await HubConnection.SendAsync("SendMessage", message);
+                }
+                else
+                {
+                    isExit = true;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
